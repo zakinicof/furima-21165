abf1f0431060e2988bf103dc307af6bf20f9a22c
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-10-30 11:04:20 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' , length: { minimum: 6, }
-  validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true do
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :nickname
     validates :first_name
     validates :last_name
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ンー－]+\z/, message: '全角カタカナを使用してください' } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ァ-ンー－]+\z/, message: '全角カタカナを使用してください' } do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' , length: { minimum: 6, }
 end
