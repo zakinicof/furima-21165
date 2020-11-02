d538de9d86f1155f8e3f1fa93c4b0a68ed408bb6
zakinicof <72780750+zakinicof@users.noreply.github.com>
2020-11-02 15:04:12 +0900

    条件分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,8 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
